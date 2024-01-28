672275794801ad8ecc10ecb6e475ee8eb83bafad
liheng.peng <liheng.peng@okg.com>
2024-01-28 15:40:01 +0800

    feat: commit code

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                index: "./index.html",
+                2048: "./2048/index.html",
+                BreakoutPC: "./BreakoutPC/index.html",
+                BreakoutMap: "./BreakoutPC/mapCreate.html",
+                Snake: "./Snake/index.html",
+                Tetris: "./Tetris/index.html",
+            },
+        },
+    },
 });
