280f61e1f1c8b007f6600d0f64948cdc0436ffc1
liheng.peng <liheng.peng@okg.com>
2024-01-28 15:38:54 +0800

    feat: finished

--- Tetris/main.tsx
+++ Tetris/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 export default App;
 
-const root = document.getElementById("root");
-
-if (!root) throw new Error("Root element not found");
+const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
     <React.StrictMode>
